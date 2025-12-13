e5b8f80c9b47508c0119e39c5fdf49c875e44c84
DennisDon678 <dennisdon678@gmail.com>
2025-12-12 20:30:42 -0600

    feat: initialize Express app with JSON body parsing, a root route, and a user router.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const UserRouter = require("./user/UserRouter");
 const app = express();
 
 app.use(express.json());
+
+// return some docs on the homepage
+app.get("/", (req, res) => {
+    res.send("<h1>Hoaxify</h1><p>This is for learning purposes</p>");
+});
 app.use("/api/v1/users", UserRouter);
 
 module.exports = app;
